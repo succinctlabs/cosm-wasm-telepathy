e9eb3e53d87c98e14cbd2b866c4b9746dcc89bb8
ratankaliani <ratankaliani@berkeley.edu>
2023-01-23 15:04:25 -0800

    refactor: remove redundant dir

--- succinct-cosmwasm/contracts/light-client/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod contract;
-mod error;
-pub mod helpers;
-pub mod integration_tests;
-pub mod msg;
-pub mod state;
-pub mod verifier;
-
-pub use crate::error::ContractError;
--- succinct-cosmwasm/ts/sdk/README.md
+++
@@ -1,1 +0,0 @@
-# succinct-cosmwasm-sdk
